8f69737d95e64682f84de22315e39c060c528759
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-04-25 11:22:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Rkn_Bots(object):
     # Rkn client config  ( required.. 😥)
     API_ID = os.environ.get("API_ID", "23631217")
     API_HASH = os.environ.get("API_HASH", "567c6df308dc6901790309499f729d12")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8153208191:AAET_YAHGjeLFNXf0q1Wy1zWVoGNwcyMnlY")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     # start_pic
     RKN_PIC = os.environ.get("RKN_PIC", "https://i.postimg.cc/8C15CQ5y/1.png https://i.postimg.cc/gcNtrv0m/2.png https://i.postimg.cc/cHD71BBz/3.png https://i.postimg.cc/F1XYhY8q/4.png https://i.postimg.cc/1tNwGVxC/5.png https://i.postimg.cc/dtW30QpL/6.png https://i.postimg.cc/139dvs3c/7.png https://i.postimg.cc/QtXVtB8K/8.png https://i.postimg.cc/y8j8G1XV/9.png https://i.postimg.cc/zDF6KyJX/10.png https://i.postimg.cc/fyycVqzd/11.png https://i.postimg.cc/26ZBtBZr/13.png https://i.postimg.cc/PJn8nrWZ/14.png https://i.postimg.cc/cC7txyhz/15.png https://i.postimg.cc/kX9tjGXP/16.png https://i.postimg.cc/zXjH4NVb/17.png https://i.postimg.cc/sggGrLhn/18.png https://i.postimg.cc/y8pgYTh7/19.png")
